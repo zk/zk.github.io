585f177143d42e76f195e4e812b05453c757f58a
Zachary Kim <zachary.kim@gmail.com>
2015-08-23 00:58:05 -0700

    update site

--- cljs/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [zkimcom.site] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "dev", :on-jsload (fn [& x] (if js/zkimcom.site.reload-hook (apply js/zkimcom.site.reload-hook x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'zkimcom.site/reload-hook' is missing"))), :websocket-url "ws://localhost:3449/figwheel-ws"})
-
